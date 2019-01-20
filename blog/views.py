a97f68a19ac20380d1f1ca2b0c14b0a2b78d0737
李常昱 <lcy19961018@163.com>
2019-01-20 21:09:51 +0800

    No.16 annotation

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.core.paginator import Paginator
 from .models import Blog, BlogType
 from django.conf import settings
+from django.db.models import Count
 
 
 # Create your views here.
@@ -25,13 +26,21 @@ def get_blog_list_common_data(blogs_all_list, request):
     if page_range[-1] != paginator.num_pages:
         page_range.append(paginator.num_pages)
 
+    # 获取日期对应统计
+    blog_dates = Blog.objects.dates('created_time', 'month', order="DESC")
+    blog_dates_dict = {}
+    for blog_date in blog_dates:
+        blog_count = Blog.objects.filter(created_time__year=blog_date.year, created_time__month=blog_date.month).count()
+        blog_dates_dict[blog_date] = blog_count
+
     context = {}
     context['blogs'] = page_of_blogs.object_list
     context['page_of_blogs'] = page_of_blogs
     context['page_range'] = page_range
-    context['blog_types'] = BlogType.objects.all()
-    context['blog_dates'] = Blog.objects.dates('created_time', 'month', order='DESC')
+    context['blog_types'] = BlogType.objects.annotate(blog_count=Count('blog'))
+    context['blog_dates'] = blog_dates_dict
     return context
+
 
 def blog_list(request):
     blogs_all_list = Blog.objects.all()
