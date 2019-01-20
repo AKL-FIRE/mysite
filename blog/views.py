aba41f9218060b92169417388acfa1d04f6e7742
李常昱 <lcy19961018@163.com>
2019-01-20 18:12:10 +0800

    No.13 add pages

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Blog, BlogType
 
 # Create your views here.
 def blog_list(request):
+    page_num = request.GET.get('page', 1) #获取页码参数(GET请求)
+    blogs_all_list = Blog.objects.all()
+    paginator = Paginator(blogs_all_list, 10) #每十页进行分页
+    page_of_blogs = paginator.get_page(page_num)
+
     context = {}
-    context['blogs'] = Blog.objects.all()
+    context['page_of_blogs'] = page_of_blogs
     context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
 
