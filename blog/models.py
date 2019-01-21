5befff0dbfdde3e92ddb9cf31269220b37c396a4
李常昱 <lcy19961018@163.com>
2019-01-21 15:04:06 +0800

    No.19 count read2

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.contenttypes.models import ContentType
+from read_statistics.models import ReadNum, ReadNumExpandMethod
+
 
 class BlogType(models.Model):
     type_name = models.CharField(max_length=15)
@@ -10,17 +13,20 @@ class BlogType(models.Model):
 
 
 # Create your models here.
-class Blog(models.Model):
+class Blog(models.Model, ReadNumExpandMethod):
     title = models.CharField(max_length=50)
     blog_type = models.ForeignKey(BlogType, on_delete=models.DO_NOTHING)
     content = RichTextUploadingField()
     author = models.ForeignKey(User,on_delete=models.DO_NOTHING)
-    read_num = models.IntegerField(default=0)
     created_time = models.DateTimeField(auto_now_add=True)
     last_update_time = models.DateTimeField(auto_now=True)
+
+
+
 
     def __str__(self):
         return "<Blog: %s>" % self.title
 
     class Meta:
         ordering = ['-created_time']
+
