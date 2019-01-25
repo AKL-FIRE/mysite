cd3e71e696f2b0a4f065c2e10c0750034b02b462
李常昱 <lcy19961018@163.com>
2019-01-25 16:44:56 +0800

    No.27 comment tree(II)

--- comment/models.py
+++ comment/models.py
@@ -5,17 +5,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Comment(models.Model):
-    content_type = models.ForeignKey(ContentType, on_delete=models.DO_NOTHING)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
 
     text = models.TextField()
     comment_time = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
 
-    root = models.ForeignKey('self', null=True, on_delete=models.DO_NOTHING, related_name='root_comment')
-    parent = models.ForeignKey('self', null=True, on_delete=models.DO_NOTHING, related_name='parent_comment')
-    reply_to = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, related_name="replies")
+    root = models.ForeignKey('self', null=True, on_delete=models.CASCADE, related_name='root_comment')
+    parent = models.ForeignKey('self', null=True, on_delete=models.CASCADE, related_name='parent_comment')
+    reply_to = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="replies")
 
     def __str__(self):
         return self.text
