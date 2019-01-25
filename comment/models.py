695925d5926e0cdb4447555d60bc83891bf032a7
李常昱 <lcy19961018@163.com>
2019-01-25 15:09:31 +0800

    No.26 comment tree

--- comment/models.py
+++ comment/models.py
@@ -11,7 +11,15 @@ class Comment(models.Model):
 
     text = models.TextField()
     comment_time = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="comments")
+
+    root = models.ForeignKey('self', null=True, on_delete=models.DO_NOTHING, related_name='root_comment')
+    parent = models.ForeignKey('self', null=True, on_delete=models.DO_NOTHING, related_name='parent_comment')
+    reply_to = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, related_name="replies")
+
+    def __str__(self):
+        return self.text
 
     class Meta:
         ordering = ['-comment_time']
+
