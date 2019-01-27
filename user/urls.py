d6a6667c6f3d0106462c2052abeaad1e52134fa7
李常昱 <lcy19961018@163.com>
2019-01-27 16:43:50 +0800

    No.32 email

--- user/urls.py
+++ user/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('login_for_modal', views.login_for_modal, name='login_for_modal'),
     path('logout/', views.logout, name='logout'),
     path('user_info/', views.user_info, name='user_info'),
+    path('change_nickname/', views.change_nickname, name='change_nickname'),
+    path('bind_email/', views.bind_email, name='bind_email'),
+    path('send_verification_code/', views.send_verification_code, name='send_verification_code'),
 ]
